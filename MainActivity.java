80428a1c728b60804e27a9e4d2df95a6c378ec49
maju20020116 <101619373+maju20020116@users.noreply.github.com>
2022-10-15 16:52:01 +0900

    2_7

--- MainActivity.java
+++ MainActivity.java
@@ -1,5 +1,6 @@
-package com.example.a21173004_2_6;
+package com.example.a21173004_2_7;
 
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -7,23 +8,24 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
     myDBHelper myDBHelper;
-    EditText edtName;
-    EditText edtPhoneNumber;
-    EditText edtEmailAddress;
-    EditText edtAddress;
-    EditText edtSecure;
-    EditText edtNameResult, edtPhoneNumberResult, edtEmailAddressResult, edtSecureResult, edtAddressResult;
-    Button btnInit, btnInsert, btnSelect;
     SQLiteDatabase sqlDB;
+    TextView jinputCount;
+    EditText jinputMessage;
+    Button asendBtn,acloseBtn;
+    Button btnInit,btnInsert,btnSelect;
+    EditText smsResult;
 
 
     @Override
@@ -31,41 +33,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edtName = findViewById(R.id.edtName);
-        edtPhoneNumber = findViewById(R.id.edtPhoneNumber);
-        edtEmailAddress = findViewById(R.id.edtEmailAddress);
-        edtAddress = findViewById(R.id.edtAddress);
-        edtSecure = findViewById(R.id.edtSecure);
+        jinputCount = findViewById(R.id.ainputCount);
+        jinputMessage = findViewById(R.id.ainputMessage);
 
-        btnInit = (Button) findViewById(R.id.btnInit);
-        btnInsert = (Button) findViewById(R.id.btnInsert);
-        btnSelect = (Button) findViewById(R.id.btnSelect);
-
-        edtNameResult = findViewById(R.id.edtNameResult);
-        edtPhoneNumberResult = findViewById(R.id.edtPhoneNumberResult);
-        edtEmailAddressResult = findViewById(R.id.edtEmailAddressResult);
-        edtAddressResult = findViewById(R.id.edtAddressResult);
-        edtSecureResult = findViewById(R.id.edtSecureResult);
+        btnInit = findViewById(R.id.btnInit);
+        btnInsert= findViewById(R.id.btnInsert);
+        btnSelect = findViewById(R.id.btnSelect);
+        smsResult =findViewById(R.id.smsResult);
+        asendBtn = findViewById(R.id.asendBtn);
 
 
-        myDBHelper = new myDBHelper(this);
-        btnInit.setOnClickListener(new View.OnClickListener() {
+        myDBHelper= new  myDBHelper(this);
+        btnInit.setOnClickListener(new View.OnClickListener() { //초기화
             @Override
             public void onClick(View view) {
                 sqlDB = myDBHelper.getWritableDatabase();
-                myDBHelper.onUpgrade(sqlDB, 1, 2);
+                myDBHelper.onUpgrade(sqlDB,1,2);
                 sqlDB.close();
             }
         });
-        btnInsert.setOnClickListener(new View.OnClickListener() { //조회
+        btnInsert.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("WrongConstant")
             @Override
             public void onClick(View view) {
-                sqlDB = myDBHelper.getWritableDatabase();
-
-                sqlDB.execSQL("INSERT INTO USER1 VALUES ('" + edtName.getText().toString() + "' ,' " + edtPhoneNumber.getText().toString() + "' , '" + edtEmailAddress.getText().toString() + "', " + edtSecure.getText().toString() + ",'" + edtAddress.getText().toString() + "');");
+                sqlDB =myDBHelper.getWritableDatabase();
+                sqlDB.execSQL("INSERT INTO sms VALUES ('"+jinputMessage.getText().toString()+"');");
                 sqlDB.close();
-                Toast.makeText(getApplicationContext(), "입력됨", 0).show();
+                Toast.makeText(getApplicationContext(),"입력됨",0).show();
 
             }
         });
@@ -74,53 +68,93 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 sqlDB = myDBHelper.getReadableDatabase();
                 Cursor cursor;
-                cursor = sqlDB.rawQuery("SELECT * FROM user1;", null);
+                cursor = sqlDB.rawQuery("SELECT * FROM sms;",null);
 
-                String strNames = "이름" + "\r\n" + "\r\n";
-                String strPhoneNumbers = "핸드폰번호" + "\r\n" + "\r\n";
-                String strEmailAddress = "이메일주소" + "\r\n" + "\r\n";
-                String strSecure = "비밀번호" + "\r\n" + "\r\n";
-                String strAddress = "주소" + "\r\n" + "\r\n";
+                String strsmsResult = "메세지"+"\r\n"+"\r\n";
 
 
-                while (cursor.moveToNext()) {
-                    strNames += cursor.getString(0) + "\r\n";
-                    strPhoneNumbers += cursor.getString(1) + "\r\n";
-                    strEmailAddress += cursor.getString(2) + "\r\n";
-                    strSecure += cursor.getString(3) + "\r\n";
-                    strAddress += cursor.getString(4) + "\r\n";
 
+                while (cursor.moveToNext()){
+                    strsmsResult+= cursor.getString(0)+"\r\n";
 
                 }
-                edtNameResult.setText(strNames);
-                edtPhoneNumberResult.setText(strPhoneNumbers);
-                edtEmailAddressResult.setText(strEmailAddress);
-                edtSecureResult.setText(strSecure);
-                edtAddressResult.setText(strAddress);
 
-
+                smsResult.setText(strsmsResult);
                 cursor.close();
                 sqlDB.close();
+            }
+        });
+
+
+
+        asendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
             }
         });
+        Button jcloseButton = findViewById(R.id.acloseBtn);
+        jcloseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        TextWatcher watcher =new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence str, int i, int i1, int i2) {
+                byte[] bytes = null;
+                try {
+                    bytes = str.toString().getBytes("KSC5601");
+                    int strCount = bytes.length;
+                    jinputCount.setText(strCount+"/80바이트");
+
+                } catch (UnsupportedEncodingException ex){
+                    ex.printStackTrace();
+                }
+            }
+
+
+
+            @Override
+            public void afterTextChanged(Editable strEditable) {
+                String str = strEditable.toString();
+                try{
+                    byte[] strBytes = str.getBytes("KSC5601");
+                    if(strBytes.length > 80){
+                        strEditable.delete(strEditable.length()-2,strEditable.length()-1);
+                    }
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+
+            }
+        };
+        jinputMessage.addTextChangedListener(watcher);
+
     }
 
     public class myDBHelper extends SQLiteOpenHelper {
-        public myDBHelper(Context context) {
-            super(context, "groupDB", null, 1);
-        }
+    public myDBHelper(Context context) {
+        super(context, "groupDB", null, 1);
+    }
 
-        @Override
-        public void onCreate(SQLiteDatabase db) {
-            db.execSQL("CREATE TABLE user1 ( gName CHAR(20), gPhoneNumber CHAR(20), gEmailAddress CHAR(40) PRIMARY KEY,gSecure INTEGER, gAddress CHAR(100));");
-        }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE sms ( gmessage CHAR(80));");
+    }
 
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            db.execSQL("DROP TABLE IF EXISTS user1");
-            onCreate(db);
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS sms");
+        onCreate(db);
 
+    }
         }
     }
-}
+
